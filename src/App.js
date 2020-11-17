96cd58abb9499fee5a78220221a74e524a4b1e93
hoangtatthanh <thanhhtth1807034@fpt.edu.vn>
2020-11-17 16:40:24 +0700

    update add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import React from "react";
+import React, {Component} from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 
 import TopNav from "./components/TopNav";
 
-function App() {
-    return (
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: []
+        }
+    }
 
-        <Router>
-            <div className="container">
-                <div className="row">
-                    <Switch>
-                        <Route path="/" component={TopNav} />
-                    </Switch>
+    s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    generateID() {
+        return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + this.s4() + this.s4();
+    }
+
+    onSubmit = (data) => {
+        // if (localStorage.getItem('notes') === null){
+        //     let notes = [];
+        //     notes.push(data);
+        //     localStorage.setItem('notes', JSON.stringify(notes));
+        // } else {
+        //     let notes = JSON.parse(localStorage.getItem('notes'));
+        //     notes.push(data);
+        //     localStorage.setItem('notes', JSON.stringify(notes));
+        // }
+        let {notes} = this.state;
+        if (data.id === ''){
+            data.id = this.generateID();
+            notes.push(data);
+        } else {
+            const index = this.findIndex(data.id);
+            notes[index] = data;
+        }
+
+        this.setState({
+            notes: notes
+        });
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }
+
+    findIndex = (id) => {
+        let { notes } = this.state;
+        let result = -1;
+        notes.forEach((note, index) => {
+            if (note.id === id){
+                result = index
+            }
+        });
+        return result;
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <div className="row">
+                        <Switch>
+                            <Route path="/">
+                                <TopNav onSubmit={this.onSubmit}/>
+                            </Route>
+                        </Switch>
+                    </div>
                 </div>
-            </div>
-        </Router>
-
-    );
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -4,17 +4,29 @@ class TopNav extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            notes: [],
-            str : 0
+            notes: {
+                id:'',
+                date:'',
+                content: ''
+            },
+            str: 0
         }
     }
 
     componentWillMount() {
         if (localStorage && localStorage.getItem('notes')) {
-            var notes = JSON.parse(localStorage.getItem('notes'));
+            let notes = JSON.parse(localStorage.getItem('notes'));
             this.setState({
                 notes: notes
             });
+        }
+    }
+
+    componentDidMount() {
+        let check = window.localStorage.getItem('notes');
+        check = JSON.parse(check);
+        if (Array.isArray(check) && check.length > 0) {
+            this.setState({ notes: check });
         }
     }
 
@@ -26,89 +38,89 @@ class TopNav extends Component {
         return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4() + this.s4() + this.s4();
     }
 
-    onGenerateData = () => {
-        const notes = [
-            {
-                id: this.generateID(),
-                date: "15/11/2020",
-                content: "note 1"
-            },
-            {
-                id: this.generateID(),
-                date: "16/11/2020",
-                // date: date.toLocaleString(),
-                content: "note 2"
-            },
-            {
-                id: this.generateID(),
-                // date: date.toLocaleString(),
-                date: "17/11/2020",
-                content: "note 3"
-            },
-            {
-                id: this.generateID(),
-                date: "14/11/2020",
-                // date: date.setDate(date.getDate() + 1),
-                content: "note 4"
-            },
-            {
-                id: this.generateID(),
-                date: "15/11/2020",
-                content: "note 5"
-            },
-            {
-                id: this.generateID(),
-                date: "16/11/2020",
-                // date: date.toLocaleString(),
-                content: "note 6"
-            },
-            {
-                id: this.generateID(),
-                // date: date.toLocaleString(),
-                date: "17/11/2020",
-                content: "note 7"
-            },
-            {
-                id: this.generateID(),
-                date: "14/11/2020",
-                // date: date.setDate(date.getDate() + 1),
-                content: "note 8"
-            },
-            {
-                id: this.generateID(),
-                date: "14/11/2020",
-                // date: date.setDate(date.getDate() + 1),
-                content: "note 9"
-            },
-            {
-                id: this.generateID(),
-                date: "15/11/2020",
-                content: "note 10"
-            },
-            {
-                id: this.generateID(),
-                date: "16/11/2020",
-                // date: date.toLocaleString(),
-                content: "note 11"
-            },
-            {
-                id: this.generateID(),
-                // date: date.toLocaleString(),
-                date: "17/11/2020",
-                content: "note 12"
-            },
-            {
-                id: this.generateID(),
-                date: "16/11/2020",
-                // date: date.setDate(date.getDate() + 1),
-                content: "note 13"
-            },
-        ];
-        this.setState({
-            notes: notes
-        });
-        localStorage.setItem("notes", JSON.stringify(notes));
-    };
+    // onGenerateData = () => {
+    //     const notes = [
+    //         {
+    //             id: this.generateID(),
+    //             date: "15/11/2020",
+    //             content: "note 1"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "16/11/2020",
+    //             // date: date.toLocaleString(),
+    //             content: "note 2"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             // date: date.toLocaleString(),
+    //             date: "17/11/2020",
+    //             content: "note 3"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "14/11/2020",
+    //             // date: date.setDate(date.getDate() + 1),
+    //             content: "note 4"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "15/11/2020",
+    //             content: "note 5"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "16/11/2020",
+    //             // date: date.toLocaleString(),
+    //             content: "note 6"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             // date: date.toLocaleString(),
+    //             date: "17/11/2020",
+    //             content: "note 7"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "14/11/2020",
+    //             // date: date.setDate(date.getDate() + 1),
+    //             content: "note 8"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "14/11/2020",
+    //             // date: date.setDate(date.getDate() + 1),
+    //             content: "note 9"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "15/11/2020",
+    //             content: "note 10"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "16/11/2020",
+    //             // date: date.toLocaleString(),
+    //             content: "note 11"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             // date: date.toLocaleString(),
+    //             date: "17/11/2020",
+    //             content: "note 12"
+    //         },
+    //         {
+    //             id: this.generateID(),
+    //             date: "16/11/2020",
+    //             // date: date.setDate(date.getDate() + 1),
+    //             content: "note 13"
+    //         },
+    //     ];
+    //     this.setState({
+    //         notes: notes
+    //     });
+    //     localStorage.setItem("notes", JSON.stringify(notes));
+    // };
 
     onNext = () => {
         this.setState({
@@ -122,11 +134,26 @@ class TopNav extends Component {
         })
     }
 
+    onChange = (event) => {
+        let target = event.target;
+        let name = target.name;
+        let value = target.value;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.notes);
+        this.props.onSubmit(this.state.notes);
+    }
+
     render() {
         let date = new Date();
         const {notes, str} = this.state;
         console.log(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`);
-        var elmNotes = notes.filter(note => note.date === `${date.getDate() + str}/${date.getMonth() + 1}/${date.getFullYear()}`).map((note) => {
+        let elmNotes = notes.filter(note => note.date === `${date.getDate() + str}/${date.getMonth() + 1}/${date.getFullYear()}`).map((note) => {
             return <span className="text-sm-left ">
                 <ul key={note.id}>
                 <li>{note.content}</li>
@@ -142,7 +169,7 @@ class TopNav extends Component {
                 </button>
                 <button className="btn btn-default col-3 mr-85 mt-3">
                     {
-                    `${date.getDate() + str}/${date.getMonth() + 1}/${date.getFullYear()}`
+                        `${date.getDate() + str}/${date.getMonth() + 1}/${date.getFullYear()}`
                     }
                 </button>
                 <button className="btn btn-default col-3 mt-3"
@@ -151,9 +178,31 @@ class TopNav extends Component {
                     Next
                 </button>
 
-                <button className="btn btn-default mt-2" onClick={this.onGenerateData}>
-                    Generate data
-                </button>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <input type="text"
+                               className="form-control"
+                               name="date"
+                               value={this.state.notes.date}
+                               onChange={this.onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Content: </label>
+                        <input type="text"
+                               className="form-control"
+                               name="content"
+                               value={this.state.notes.content}
+                               onChange={this.onChange}
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-warning">
+                        <span className="fa fa-plus mr-5"></span>Luu
+                    </button>
+                </form>
+
                 <div className="m-30 bg-white">
                     {elmNotes}
                 </div>
